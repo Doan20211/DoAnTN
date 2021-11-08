f9e8e0623e9f846a67deedfc43679d47664740c4
hien <vph1997@gmail.com>
2021-11-08 21:17:15 +0700

    update admin service

--- src/main/java/com/DoAn/DoAnTotNghiep/Service/AdminService.java
+++ src/main/java/com/DoAn/DoAnTotNghiep/Service/AdminService.java
@@ -1,4 +1,16 @@
 package com.DoAn.DoAnTotNghiep.Service;
 
+import com.DoAn.DoAnTotNghiep.DTO.Request.AccountDTORe;
+import com.DoAn.DoAnTotNghiep.DTO.Response.AccountDTO;
+import com.DoAn.DoAnTotNghiep.Entity.Account;
+
+import java.util.List;
+
 public interface AdminService {
+
+    boolean createAccount(Account account);
+    boolean deleteAccount(Long id);
+    List<AccountDTO> getAllAccount();
+    AccountDTO getAccountById(Long id);
+    boolean updateAccount(AccountDTORe accountDTORe, Long id);
 }
--- src/main/java/com/DoAn/DoAnTotNghiep/Service/Impl/AdminServiceImpl.java
+++ src/main/java/com/DoAn/DoAnTotNghiep/Service/Impl/AdminServiceImpl.java
@@ -1,6 +1,54 @@
 package com.DoAn.DoAnTotNghiep.Service.Impl;
 
+import com.DoAn.DoAnTotNghiep.DTO.Request.AccountDTORe;
+import com.DoAn.DoAnTotNghiep.DTO.Response.AccountDTO;
+import com.DoAn.DoAnTotNghiep.Entity.Account;
+import com.DoAn.DoAnTotNghiep.Repository.AccountRepository;
 import com.DoAn.DoAnTotNghiep.Service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class AdminServiceImpl implements AdminService {
+
+    @Autowired
+    AccountRepository accountRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Override
+    public boolean createAccount(Account account) {
+        account.setPassword(passwordEncoder.encode("111111"));
+        accountRepository.save(account);
+        return true;
+    }
+
+    @Override
+    public boolean deleteAccount(Long id) {
+        accountRepository.deleteById(id);
+        return true;
+    }
+
+    @Override
+    public List<AccountDTO> getAllAccount() {
+        List<Account> list = accountRepository.findAll();
+        return list.stream()
+                .map(account -> new AccountDTO(account))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public AccountDTO getAccountById(Long id) {
+        Account account = accountRepository.getById(id);
+        return new AccountDTO(account);
+    }
+
+    @Override
+    public boolean updateAccount(AccountDTORe accountDTORe, Long id) {
+        return false;
+    }
 }
